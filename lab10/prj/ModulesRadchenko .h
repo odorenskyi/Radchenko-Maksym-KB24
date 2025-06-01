43e267082b895bed61bee3110c017cea29722600
Maksym Radchenko <maksimxxxx86gta@gmail.com>
2025-06-01 11:08:02 +0200

    Compiled task 10

---
+++ lab10/prj/ModulesRadchenko .h
@@ -0,0 +1,25 @@
+#ifndef MODULESRADCHENKO_H_INCLUDED
+#define MODULESRADCHENKO_H_INCLUDED
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+float s_calculation(int x, int y, int z);
+
+string s_calculation_to_string(int x, int y, int z);
+
+int task_9_1(int salary, int work_exp);
+
+string head_task_9_1(int salary, int work_exp);
+
+string task_9_2(int size_head);
+
+int task_9_3(int number);
+
+string test_module(bool result);
+
+#endif
