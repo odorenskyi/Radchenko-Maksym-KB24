5eee4ecbdb16560f386abf9f4c3324dd809368bb
Maksym Radchenko <maksimxxxx86gta@gmail.com>
2025-06-02 19:01:16 +0200

    ModulesRadchenko .h

--- lab10/prj/ModulesRadchenko .h
+++ lab10/prj/ModulesRadchenko .h
@@ -9,17 +9,6 @@
 using namespace std;
 
 float s_calculation(int x, int y, int z);
-
 string s_calculation_to_string(int x, int y, int z);
 
-int task_9_1(int salary, int work_exp);
-
-string head_task_9_1(int salary, int work_exp);
-
-string task_9_2(int size_head);
-
-int task_9_3(int number);
-
-string test_module(bool result);
-
-#endif
+#endif // MODULESRADCHENKO_H_INCLUDED
