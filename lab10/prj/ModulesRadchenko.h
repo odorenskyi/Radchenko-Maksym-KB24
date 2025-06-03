29e3fe316b63f2aea213c8292ba913d317ad7e4d
Maksym Radchenko <maksimxxxx86gta@gmail.com>
2025-06-03 05:55:47 +0200

    ModulesRadchenko.h

--- lab10/prj/ModulesRadchenko.h
+++ lab10/prj/ModulesRadchenko.h
@@ -4,11 +4,25 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <bitset>
 #include <windows.h>
 
 using namespace std;
 
-float s_calculation(int x, int y, int z);
-string s_calculation_to_string(int x, int y, int z);
+// Функція для обчислення якогось значення (наприклад, середнє арифметичне)
+float s_calculation(int x, int y, int z) {
+    return (x + y + z) / 3.0f;
+}
+
+// Функція, яка повертає обчислення у вигляді рядка
+string s_calculation_to_string(int x, int y, int z) {
+    float result = s_calculation(x, y, z);
+    return "Result: " + to_string(result);
+}
+
+// Функція для представлення числа у двійковому коді (для числа b)
+string to_binary(int b) {
+    return bitset<32>(b).to_string();
+}
 
 #endif // MODULESRADCHENKO_H_INCLUDED
